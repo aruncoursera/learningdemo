538e988de94e06ad252f479d0a7e9b6c1081e213
Arun Ramanathan <Arun.Ramanathan@microfocus.com>
2018-07-19 22:30:56 +0530

    build script changes

--- HiddenMessage/build.sh
+++ HiddenMessage/build.sh
@@ -1,19 +1,33 @@
+set -x
+set -e
+
+################################################
+quit_on_error () {
+	echo "Status : $1"
+	if ! [[ $1 == *200*OK* ]]; then
+        echo "Failed Status : $1"
+        exit 1
+	fi
+
+}
+################################################
+
 rm -rf bin/*
 rm -rf etc
 rm -rf output/*
 
-javac -cp .:bin -d bin/ src/src/mf/com/*.java
-javac -cp .:bin -d bin/ src/src/mf/com/MessageCreator.java
-javac -cp .:bin -d bin/ src/src/mf/com/FileGenerator.java
-javac -cp .:bin -d bin/ src/src/mf/com/CreateContent.java
+## Build Code
+javac -cp .:bin:ext/* -d bin/ src/src/mf/com/*.java
 
-javac -cp .:bin:ext/* -d bin/ src/src/mf/com/TestMessageCreator.java
+##Unit Test
 java -cp .:bin:ext/* org.junit.runner.JUnitCore src.mf.com.TestMessageCreator
 
-
+## Create Content
 java -cp .:bin:ext/* src.mf.com.CreateContent
 
+## Package Content
 tar -cvf hiddenmessages.tar etc/
 
+## Place Content
 mkdir -p output/
 cp hiddenmessages.tar  output/
